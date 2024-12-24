eaad2787eec04fb13087dd498f730037393004b4
Arshlxxx <leileihexxx@gmail.com>
2024-12-24 11:54:10 +0800

    add

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nvidia/tf_ws/src
-# Build directory: /home/nvidia/tf_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("tf_listener")
